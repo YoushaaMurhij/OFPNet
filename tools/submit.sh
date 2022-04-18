b96e23f66c8241171b759f295b0493601a693e61
youshaamurhij <yosha.morheg@gmail.com>
2022-04-18 10:21:48 +0300

    added inferece to compress

--- tools/submit.sh
+++ tools/submit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Running inference script on Val/Test set"
+python3 ./tools/submit.py --method R2Att_UNet --description 'R2Att_UNet model on 4 epoch'
+
 echo "Compressing validation results for submission ..."
 tar czvf /home/workspace/Occ_Flow_Pred/data/Waymo_Motion/waymo_open_dataset_motion_v_1_1_0/uncompressed/occupancy_flow_challenge/submit_validation.tar.gz  \
       -C /home/workspace/Occ_Flow_Pred/data/Waymo_Motion/waymo_open_dataset_motion_v_1_1_0/uncompressed/occupancy_flow_challenge/validation .
